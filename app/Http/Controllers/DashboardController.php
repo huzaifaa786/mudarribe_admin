ace6a6bdd9d010c1119c73021e08f67d59252ca5
Huzaifa <tritechuzaifa@gmail.com>
2024-01-25 15:23:53 +0500

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,11 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index',['tab'=>'dashboard']);
+        $firestore = app('firebase.firestore');
+        $trainer = $firestore->database()->collection('users')->where('userType', '=', 'trainer')->documents()->size();
+        $trainee = $firestore->database()->collection('users')->where('userType', '=', 'trainee')->documents()->size();
+        $total_user = $trainee + $trainer;
+
+        return view('dashboard.index',['tab'=>'dashboard', 'trainer'=> $trainer, 'trainee'=>$trainee, 'total'=>$total_user]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,7 +8,7 @@
         <div class="flex flex-row">
             <div class="basis-9/12 p-4">
                 <p class="text-base font-semibold">Total Trainers</p>
-                <h5 class="text-lg font-bold text-black">500</h5>
+                <h5 class="text-lg font-bold text-black">{{$trainer}}</h5>
             </div>
             <div class="basis-3/12 p-4">
                <div class="bg-gradient-to-tl from-purple-700 to-pink-500 text-white rounded-xl w-12 h-12 text-center py-3">
@@ -22,7 +22,7 @@
         <div class="flex flex-row">
             <div class="basis-9/12 p-4">
                 <p class="text-base font-semibold">Total Trainee</p>
-                <h5 class="text-lg font-bold text-black">500</h5>
+                <h5 class="text-lg font-bold text-black">{{$trainee}}</h5>
             </div>
             <div class="basis-3/12 p-4">
                <div class="bg-gradient-to-tl from-purple-700 to-pink-500 text-white rounded-xl w-12 h-12 text-center py-3">
@@ -35,8 +35,8 @@
     <div class="bg-white rounded-xl shadow-lg">
         <div class="flex flex-row">
             <div class="basis-9/12 p-4">
-                <p class="text-base font-semibold">Total Trainers</p>
-                <h5 class="text-lg font-bold text-black">500</h5>
+                <p class="text-base font-semibold">Total Users</p>
+                <h5 class="text-lg font-bold text-black">{{$total}}</h5>
             </div>
             <div class="basis-3/12 p-4">
                <div class="bg-gradient-to-tl from-purple-700 to-pink-500 text-white rounded-xl w-12 h-12 text-center py-3">
